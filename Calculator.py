4fcbc1eb7207edec4e355807e98a858fb3d81bb2
Tanumoy Bose <tanumoybose1986@gmail.com>
2023-02-21 17:15:02 +0530

    Add files via upload
    updated

--- Calculator.py
+++ Calculator.py
@@ -1,33 +1,5 @@
-#print(" welcome to Calculator")
+print("welcome to Calculator")
 
-while(True):
-
-
- try:
-    numberA = float(input("Please input 1st Number :"))
-    numberB = float(input("Please input 2nd Number :"))
- except ValueError:
-     print("Invalid input. Please enter a number.")
- break    
-
-
-print(" Please enter a operator")
-print(" 1 for Addition")
-print(" 2 for Substraction")
-print(" 3 for Multiplication")
-print(" 4 for Division")
-choise = input("Enter 1 , 2 ,3 or 4  :")
-
-if choise == "1":
-        add(numberA , numberB)
-elif choise == "2":
-        subs(numberA , numberB)
-elif choise == "3":
-        mul (numberA , numberB)
-elif choise == "4":
-        divide(numberA , numberB)
-else :
-        print("You have given a wrong input")    
 
 def add(x, y):
             result = x + y
@@ -45,4 +17,40 @@ def divide(x, y):
         except ZeroDivisionError:
             print("Sorry ! You are dividing by zero ")
 
-             
+print(" Please enter a operator")
+print(" 1 for Addition")
+print(" 2 for Substraction")
+print(" 3 for Multiplication")
+print(" 4 for Division")
+choice = input("Enter 1 , 2 ,3 or 4  :")
+if choice in ('1', '2', '3', '4'):
+
+ while(True):
+
+
+  try:
+    numberA = float(input("Please input 1st Number :"))
+    numberB = float(input("Please input 2nd Number :"))
+  except ValueError:
+     print("Invalid input. Please enter a number.")
+  
+  break    
+
+ if choice == "1":
+        add(numberA , numberB)
+ elif choice == "2":
+        subs(numberA , numberB)
+ elif choice == "3":
+        mul (numberA , numberB)
+ elif choice == "4":
+        divide(numberA , numberB)
+        
+else :
+        print("You have given a wrong input")   
+
+ 
+
+
+
+
+
