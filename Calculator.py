0f8242bd609be7d63d93231ccf2699cc8855637b
Tanumoy Bose <tanumoybose1986@gmail.com>
2023-02-21 17:05:48 +0530

    Add files via upload
    updated

--- Calculator.py
+++ Calculator.py
@@ -1,22 +1,15 @@
+#print(" welcome to Calculator")
 
-numberA = float(input("Please input 1st Number :"))
-numberB = float(input("Please input 2nd Number :"))
+while(True):
 
-def add(x, y):
-        result = x + y
-        print("The reasult of  addition is ", result)
-def subs(x, y):
-        result = x - y
-        print("The reasult of  Substraction is ", result)
-def mul(x, y):
-        result = x * y
-        print("The reasult of  Multiplication is ", result)
-def divide(x, y):
-    try:
-        result = x / y
-        print("The answer is :", result)
-    except ZeroDivisionError:
-        print("Sorry ! You are dividing by zero ")
+
+ try:
+    numberA = float(input("Please input 1st Number :"))
+    numberB = float(input("Please input 2nd Number :"))
+ except ValueError:
+     print("Invalid input. Please enter a number.")
+ break    
+
 
 print(" Please enter a operator")
 print(" 1 for Addition")
@@ -26,12 +19,30 @@ print(" 4 for Division")
 choise = input("Enter 1 , 2 ,3 or 4  :")
 
 if choise == "1":
-    add(numberA , numberB)
+        add(numberA , numberB)
 elif choise == "2":
-    subs(numberA , numberB)
+        subs(numberA , numberB)
 elif choise == "3":
-    mul (numberA , numberB)
+        mul (numberA , numberB)
 elif choise == "4":
-    divide(numberA , numberB)
+        divide(numberA , numberB)
 else :
-    print("You have given a wrong input")
+        print("You have given a wrong input")    
+
+def add(x, y):
+            result = x + y
+            print("The reasult of  addition is ", result)
+def subs(x, y):
+            result = x - y
+            print("The reasult of  Substraction is ", result)
+def mul(x, y):
+            result = x * y
+            print("The reasult of  Multiplication is ", result)
+def divide(x, y):
+        try:
+            result = x / y
+            print("The answer is :", result)
+        except ZeroDivisionError:
+            print("Sorry ! You are dividing by zero ")
+
+             
